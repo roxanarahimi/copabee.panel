1619744264a304851ff6068733386f471a9c6cf9
Roxana <ms.roxanarahimi@gmail.com>
2025-08-31 03:58:12 +0330

    m

--- app/Filament/Resources/Contents/Schemas/ContentInfolist.php
+++ app/Filament/Resources/Contents/Schemas/ContentInfolist.php
@@ -16,13 +16,15 @@ class ContentInfolist
             ->components([
                 ImageEntry::make('image')->label('تصویر')  ->disk('public')->visibility('public'),
                 TextEntry::make('title')->label('عنوان')->columnStart(1)->columnSpan(1),
+                TextEntry::make('title_en')->label('عنوان انگلیسی')->columnStart(1)->columnSpan(1),
                 TextEntry::make('category.title')->label('دسته بندی')->columnSpan(1),
                 IconEntry::make('visible')
                     ->label('نمایش')
                     ->boolean()
                     ->trueColor('info')//->falseIcon('')
                     ->falseColor('danger')->columnSpan(1),
-                TextEntry::make('text')->label('متن')->html()->columnSpan(3)
+                TextEntry::make('text')->label('متن')->html()->columnSpan(3),
+                TextEntry::make('text_en')->label('متن انگلیسی')->html()->columnSpan(3)
 
 
             ]);
