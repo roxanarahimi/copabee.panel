53dba022b90be0d35a00c4eb5a3cfd539c006d63
Roxana <ms.roxanarahimi@gmail.com>
2025-09-15 22:50:11 +0330

    ll

---
+++ app/Http/Controllers/ContentController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ContentResource;
+use App\Models\Content;
+use Illuminate\Http\Request;
+
+class ContentController extends Controller
+{
+    public function index($id)
+    {
+        try {
+            $contents = Content::where('visible')->where('category_id',$id)->get();
+            return response(ContentResource::collection($contents),200);
+        }catch(\Exception $exception){
+            return $exception;
+        }
+    }
+    public function show($id)
+    {
+        try {
+            $contents = Content::where('visible')->where('category_id',$id)->get();
+            return response(ContentResource::collection($contents),200);
+        }catch(\Exception $exception){
+            return $exception;
+        }
+    }
+}
