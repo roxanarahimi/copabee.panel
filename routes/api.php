926852394a23f70bb9924c984a9aaec3f7933a83
Roxana <ms.roxanarahimi@gmail.com>
2025-09-21 23:36:20 +0330

    nn

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,9 @@
 <?php
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+    Route::controller(App\Http\Controllers\ClientSideController::class)->group(function () {
+        Route::get('/get/contents/{id}', 'contents');
+        Route::get('/get/content/{slug}', 'content');
+        Route::get('/get/banners', 'banners');
+    });
 
-Route::resource('prize', App\Http\Controllers\ClientSideController::class);
-
-Route::controller(App\Http\Controllers\ClientSideController::class)->group(function () {
-    Route::get('/get/contents/{id}', 'contents');
-    Route::get('/get/content/{slug}', 'content');
-    Route::get('/get/banners', 'banners');
-});
