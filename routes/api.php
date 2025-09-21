f838efda8546ed2e5359b990e8d251c6060f5b49
Roxana <ms.roxanarahimi@gmail.com>
2025-09-21 20:46:43 +0330

    nn

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,5 @@
 <?php
-
-use App\Http\Middleware\CorsMiddleware;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware([CorsMiddleware::class])->group(function () {
 
     Route::controller(App\Http\Controllers\ClientSideController::class)->group(function () {
         Route::get('/get/contents/{id}', 'contents');
@@ -23,4 +7,3 @@ Route::middleware([CorsMiddleware::class])->group(function () {
         Route::get('/get/banners', 'banners');
     });
 
-});
