66647b5f6e821d33032726847f4dbe412371a625
Roxana <ms.roxanarahimi@gmail.com>
2025-09-24 21:10:55 +0330

    nn.lkl

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 use Illuminate\Support\Facades\Route;
+Route::options('/{any}', function (\Illuminate\Http\Request $request) {
+    return response('', 200)
+        ->header('Access-Control-Allow-Origin', 'https://asallaziz.com')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+})->where('any', '.*');
 
     Route::controller(App\Http\Controllers\ClientSideController::class)->group(function () {
         Route::get('/get/contents/{id}', 'contents');
