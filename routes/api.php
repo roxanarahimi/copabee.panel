5d80c51199acf2619ceb855e0aa63d2666181913
Roxana <ms.roxanarahimi@gmail.com>
2025-09-26 19:43:05 +0330

    m

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,13 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-Route::options('/{any}', function (Request $request) {
-    return response('', 200)
-        ->header('Access-Control-Allow-Origin', 'https://asallaziz.com')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
-        ->header('Access-Control-Allow-Credentials', 'true');
-})->where('any', '.*');
+//Route::options('/{any}', function (Request $request) {
+//    return response('', 200)
+//        ->header('Access-Control-Allow-Origin', 'https://asallaziz.com')
+//        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+//        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
+//        ->header('Access-Control-Allow-Credentials', 'true');
+//})->where('any', '.*');
 
     Route::controller(App\Http\Controllers\ClientSideController::class)->group(function () {
         Route::get('/get/contents/{id}', 'contents');
