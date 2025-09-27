cdc255dbe2cb131e4ed1637651b1345dbe9ad178
Roxana <ms.roxanarahimi@gmail.com>
2025-09-28 01:30:56 +0330

    m

--- app/Http/Resources/ContentResource.php
+++ app/Http/Resources/ContentResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Http\Controllers\DateController;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,25 @@ class ContentResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return[
+            'id'=> $this->id,
+            'title'=> $this->title,
+            'title_en'=> $this->title_en,
+            'slug'=> $this->slug,
+            'slug_en'=> $this->slug_en,
+            'category'=> $this->category,
+            'image'=> $this->image,
+            'text'=> $this->text,
+            'text_en'=> $this->text_en,
+            'meta_description'=> $this->meta_description,
+            'meta_description_'=> $this->meta_description_,
+            'visible'=> $this->visible,
+
+            'view'=> $this->view,
+            'like'=> $this->like,
+            'dislike'=> $this->dislike,
+            'score'=> $this->score,
+            'created_at'=> explode(' ', (new DateController())->toPersian($this->created_at))[0],
+        ];
     }
 }
