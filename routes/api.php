718c873a1174da501af2bbe012097a50f75f922e
Roxana <ms.roxanarahimi@gmail.com>
2025-09-27 20:18:13 +0330

    m

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 Route::options('/{any}', function (Request $request) {
     return response('', 200)
-        ->header('Access-Control-Allow-Origin', 'https://asallaziz.com')
+        ->header('Access-Control-Allow-Origin', '*')//aa
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
         ->header('Access-Control-Allow-Credentials', 'true');
