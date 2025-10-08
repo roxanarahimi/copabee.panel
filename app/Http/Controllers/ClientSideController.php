828b60aeac1923ce9a577c1e659751b28d49c85c
Roxana <ms.roxanarahimi@gmail.com>
2025-10-08 20:48:21 +0330

    m

--- app/Http/Controllers/ClientSideController.php
+++ app/Http/Controllers/ClientSideController.php
@@ -70,7 +70,7 @@ class ClientSideController extends Controller
     public function search(Request $request)
     {
         $data = [];
-        if(strlen($request['term'])>=3){
+        if(strlen($request['term'])>3){
             $contents = Content::orderByDesc('created_at')->where('visible',1)->where('title','Like','%'.$request['term'].'%')->get();
             foreach ($contents as $item){
                 $data[]=["title"=>$item->title, "link"=> '/content'.$item->slug];
