7aaa8fea4b03e11b5dbfeb7ecb788b4d4ac42222
Roxana <ms.roxanarahimi@gmail.com>
2025-10-22 10:26:23 +0330

    forms

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,16 +5,15 @@ namespace App\Http\Controllers;
 use App\Http\Resources\UserResource;
 use App\Models\Collaboration;
 use App\Models\Complane;
-use App\Models\User;
-use Illuminate\Http\Response;
+use http\Client\Curl\User;
+use http\Message;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
+use Illuminate\Http\Response;
 
-//redis
 
 class UserController extends Controller
 {
-//    public function sendOtp(Request $request): Response|\Exception
+//    public function sendOtp(Request $request)
 //    {
 //        try {
 //            $code = rand(1001, 9999);
@@ -22,10 +21,8 @@ class UserController extends Controller
 //        کد تایید شما:
 //        ' . $code;
 //
-//            $sms = ["mobile" => $request['mobile'], "message" => $text];
-//            Cache::put($request['mobile'], $code, 60); // expires in 60 seconds
+//            $sms = ["mobie" => $request['mobile'], "text" => $text];
 //            $send = $this->sendSms($sms);
-//
 //            if ($send->status === 200) {
 //                $user = User::where('mobile', $request['mobile'])->first();
 //                if ($user && $user->role === 'admin') {
@@ -36,59 +33,59 @@ class UserController extends Controller
 //                }
 //                //save code in db ....
 //                $sms = $this->sendSms(['mobile' => $request->mobile, 'text' => $text]);
-//                if ($sms['status'] === 200) {
+//                if ($sms->status === 200) {
 //                    return response(['user' => $user, 'message' => 'کد تایید برای شما پیامک شد. لطفا در کادر زیر وارد کنید.'], 200);
 //                } else {
-//                    return response($sms,$sms['status']);
+//                    return $sms;
 //                }
 //            }
 //        } catch (\Exception $exception) {
 //            return $exception;
 //        }
-//        return response(['message' => 'خطا در ارسال پیامک. لطفا دوباره تلاش کنید.'], 500);
 //
 //    }
 
-    public function sendSms($request): Response|\Exception
+    public function sendSms(Request $request): Response
     {
         try {
             $api = new \Kavenegar\KavenegarApi("4470686233536566795848666962306F59327335574D786772655075704668586C31415162524E717747413D");
             $sender = "10005989";
-            $message = $request->message;
-            $receptor = array($request->mobile);
+            $message = "خدمات پیام کوتاه کاوه نگار";
+            $receptor = array("09128222725");
             $result = $api->Send($sender, $receptor, $message);
             if ($result) {
                 $info = [
-                    "messageid" => $result[0]->messageid,
-                    "message" => $result[0]->message,
-                    "status" => $result[0]->status,
-                    "statustext" => $result[0]->statustext,
-                    "sender" => $result[0]->sender,
-                    "receptor" => $result[0]->receptor,
-                    "date" => $result[0]->date,
-                    "cost" => $result[0]->cost
+                    "messageid" => $result->messageid,
+                    "message" => $result->message,
+                    "status" => $result->status,
+                    "statustext" => $result->statustext,
+                    "sender" => $result->sender,
+                    "receptor" => $result->receptor,
+                    "date" => $result->date,
+                    "cost" => $result->cost
                 ];
 
-            } else {
+            }else{
                 $info = $result;
             }
             return response($info, 200);
 
-        } catch (\Exception $e) {
-            return $e;
+        } catch(\Kavenegar\Exceptions\ApiException $e){
+            // در صورتی که خروجی وب سرویس 200 نباشد این خطا رخ می دهد
+            return $e->errorMessage();
         }
-
+        catch(\Kavenegar\Exceptions\HttpException $e){
+            // در زمانی که مشکلی در برقرای ارتباط با وب سرویس وجود داشته باشد این خطا رخ می دهد
+            return $e->errorMessage();
+        }
     }
 
-    public function verifyMobile(Request $request): Response|\Exception
+    public function verifyMobile(Request $request)
     {
         try {
             $user = User::where('mobile', $request['mobile'])->first();
-            if (Cache::get($request['mobile']) === $request['code']) {
-                return response([
-                    'message' => 'شماره موبایل با موفقیت تایید شد.',
-                    'user' => $user],
-                    200);
+            if ($user->otp === $request['codemobile']) {
+                return response(['message' => 'شماره موبایل با موفقیت تایید شد.'], 200);
             } else {
                 return response(['message' => 'کد تایید وارد شده اشتباه است.'], 422);
             }
@@ -97,7 +94,7 @@ class UserController extends Controller
         }
     }
 
-    public function store(Request $request): Response|\Exception
+    public function store(Request $request)
     {
         try {
             $user = User::create($request->all());
